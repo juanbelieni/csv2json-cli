7406fb642cc90e7851ecdf8486a53d6a80158586
Juan Belieni <juanbelieni@gmail.com>
2020-02-24 16:50:32 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# CSV2JSON
+
+
+`CLI to convert CSV to JSON`
+
+Usage: csv2json [options] \<csv>
+
+
+```
+Options:
+  -V, --version                output the version number
+  -o, --output <filename>      especify the output filename (default: "output.json")
+  -d, --delimiter <delimiter>  delimiter of csv file (default: ",")
+  -h, --help                   output usage information
+```
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import program from 'commander';
+import csvParse from 'csv-parse/lib/sync';
+import fs from 'fs';
+
+function csv2json(csvPath, { output, delimiter }) {
+  const csv = fs.readFileSync(csvPath).toString();
+
+  const json = csvParse(csv, {
+    delimiter,
+  });
+
+  fs.writeFileSync(output, JSON.stringify(json));
+}
+
+program
+  .name('csv2json')
+  .description('CLI to convert CSV to JSON')
+  .version('0.0.1')
+  .arguments('<csv>')
+  .option(
+    '-o, --output <filename>',
+    'especify the output filename',
+    'output.json'
+  )
+  .option('-d, --delimiter <delimiter>', 'delimiter of csv file', ',')
+  .action(csv2json);
+
+program.parse(process.argv);
